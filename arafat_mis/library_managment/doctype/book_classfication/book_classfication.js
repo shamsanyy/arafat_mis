d85f4e68007aa097cd6c0341bc9e0d0ebe31306c
shamsanyy <prosite@Arafat-lab>
2024-01-08 13:26:01 +0300

    this is first uploaded issue

---
+++ arafat_mis/library_managment/doctype/book_classfication/book_classfication.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, arafat and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Book Classfication", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ arafat_mis/library_managment/doctype/library_beneficiary/library_beneficiary.js
@@ -0,0 +1,25 @@
+// Copyright (c) 2023, arafat and contributors
+// For license information, please see license.txt
+
+ frappe.ui.form.on("Library Beneficiary", {
+//refresh:function(frm){
+
+   // frm.set_intro('هنا قم بإضافة جميع بيانات الحقول')
+
+
+    //if(frm.is_new())
+    //{
+      //  frm.set_intro('هنا قم بإضافة جميع بيانات الحقول')
+    //}
+onload:function(frm)
+{
+frappe.msgprint("you Wellcome")
+}
+
+
+    //refresh(frm) {
+   // frappe.msgprint("اهلا عرفات كيف الحال")
+   //frappe.throw(" this is message ")
+ 
+,
+ });
